1d4384a70e4c3a607601a5d346ee520cd1c180b4
Miguel Domínguez <65074687+Dominguezd01@users.noreply.github.com>
2023-05-11 09:43:12 +0000

    .

---
+++ src/routes/user[id]/+page.svelte
@@ -0,0 +1,15 @@
+<script context="module">
+    export function load({ url }) {
+      const ref = url.searchParams.get('id')';
+      return {
+        props: {
+          ref
+        }
+      };
+    }
+  </script>
+  <script>
+    export let ref;
+  
+    // do stuff
+  </script>
