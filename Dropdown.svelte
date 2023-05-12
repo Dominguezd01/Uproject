dcbe17139573f4d2323d3f4521612565cae32f4d
Miguel Domínguez <65074687+Dominguezd01@users.noreply.github.com>
2023-05-12 10:38:16 +0200

    Add files via upload

---
+++ Dropdown.svelte
@@ -0,0 +1,156 @@
+<script>
+    import logo from '$lib/assets/UpLogoWhite.svg';
+    import user from '$lib/assets/user.svg';
+    import { browser } from '$app/environment';
+    const logOut = () =>{
+      if(browser){
+        sessionStorage.clear()
+        location.href = "/auth/login"
+      }
+    }
+    const getUserName = () =>{
+      if(browser){
+          return sessionStorage.getItem("userName")
+      }
+    }
+    export let userId
+</script>
+<link rel="preconnect" href="https://fonts.googleapis.com" />
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+<link
+  href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;700&display=swap"
+  rel="stylesheet"
+/>
+<link
+  href="https://fonts.googleapis.com/icon?family=Material+Icons+Outlined"
+  rel="stylesheet"
+/>
+
+
+<nav>
+    <ul>
+      <li class="main">
+        <span>{getUserName()}</span>
+        <img src={user} class="profile" />
+        <ul>
+          <li class="sub-item">
+            <span class="material-icons-outlined"> manage_accounts </span>
+            <a class="link" href= "/user/edit">Update Profile</a>
+          </li>
+          <li class="sub-item">
+            <span class="material-icons-outlined"> logout </span>
+            <p on:click={logOut}>Logout</p>
+          </li>
+        </ul>
+      </li>
+    </ul>
+  </nav>
+
+<style>
+img {
+  max-width: 100%;
+  height: auto;
+}
+
+nav {
+  background-color: #ff682c;
+  padding: 0 3rem;
+  border-radius: 0.625rem;
+}
+.main{
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 0,2em;
+}
+ul {
+  margin: 0;
+  padding: 0;
+  display: flex;
+  align-items: center;
+  gap: 3rem;
+}
+
+li {
+  list-style-type: none;
+  position: relative;
+  padding: 0.625rem 0 0.5rem;
+}
+li ul {
+  flex-direction: column;
+  position: absolute;
+  background-color: white;
+  align-items: flex-start;
+  transition: all 0.5s ease;
+  width: 20rem;
+  right: -3rem;
+  top: 3.5rem;
+  border-radius: 0.325rem;
+  gap: 0;
+  padding: 1rem 0rem;
+  opacity: 0;
+  box-shadow: 0px 0px 100px rgba(20, 18, 18, 0.25);
+  display: none;
+}
+
+ul li:hover > ul,
+ul li ul:hover {
+  visibility: visible;
+  opacity: 1;
+  display: flex;
+}
+
+.material-icons-outlined {
+  color: #888888;
+  transition: all 0.3s ease-out;
+}
+
+.material-icons-outlined:hover {
+  color: #ff9800;
+  transform: scale(1.25) translateY(-4px);
+  cursor: pointer;
+}
+
+
+.profile {
+  height: 3rem;
+  width: auto;
+  cursor: pointer;
+}
+
+.sub-item {
+  width: 100%;
+  display: flex;
+  align-items: center;
+  gap: 0.725rem;
+  cursor: pointer;
+  padding: 0.5rem 1.5rem;
+}
+
+.sub-item:hover .material-icons-outlined {
+  color: #ff9800;
+  transform: scale(1.08) translateY(-2px);
+  cursor: pointer;
+}
+
+.sub-item:hover p {
+  color: #ff9800;
+  cursor: pointer;
+}
+
+.sub-item p {
+  font-size: 0.85rem;
+  color: #888888;
+  font-weight: 500;
+  margin: 0.4rem 0;
+  flex: 1;
+}
+.link{
+  text-decoration: none;
+  color: #888888;
+}
+.link:hover{
+  color: #ff9800;
+}
+
+</style>
---
+++ UserInfo.svelte
@@ -0,0 +1,110 @@
+<script>
+    import user from "$lib/assets/User.svg";
+    import Swal from "sweetalert2";
+    export let userInfoData;
+    console.log(userInfoData);
+    let oldPass, userName, userEmail, newPass, userNameInput, userEmailInput;
+    userName = userInfoData.name
+    userEmail = userInfoData.email
+
+    const handleSubmit = async() =>{
+        if(oldPass.trim() == "" || newPass.trim() == ""  ){
+            Swal.fire({
+                title: "Error",
+                text: "You must type your old password to update your profile",
+                icon: "error"
+            })
+        }
+    }
+</script>
+
+<div class="mainContainer">
+
+    <div class="imageContainer">
+        <img class="profilePicture" src={user} alt="userProfilePhoto" />
+    </div>
+    <form class="editProfileForm" on:submit|preventDefault={handleSubmit}>
+        <div class="inputsContainer">
+            <div class="inputContainer">
+                <label for="userName">User name</label>
+                <input
+                    id="userName"
+                    type="text"
+                    bind:value={userName}
+                    bind:this={userNameInput}
+                    required
+                    class="input"
+                />
+            </div>
+            <div class="inputContainer">
+                <label for="email">Email</label>
+                <input
+                    type="email"
+                    bind:value={userEmail}
+                    bind:this={userEmailInput}
+                    required
+                    class="input"
+                />
+            </div>
+    
+            <div class="inputContainer">
+                <label for="password">Old password</label>
+                <input id="password" required type="password" bind:value={oldPass} class="input" />
+            </div>
+            <div class="inputContainer">
+                <label for="newPass">New password</label>
+                <input id="newPass" type="password" bind:value={newPass} class="input" />
+            </div>
+        </div>
+
+        <div>
+            <input class="submit" type="submit">
+        </div>
+    </form>
+</div>
+
+<style>
+    .profilePicture {
+        width: 200px;
+    }
+    .mainContainer {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: center;
+        gap: 2em;
+        margin-top: 100px;
+    }
+    .inputContainer {
+        display: grid;
+        place-items: center;
+    }
+    .inputsContainer {
+        display: grid;
+        grid-template-columns: repeat(2, 1fr);
+        grid-template-rows: repeat(2, 1fr);
+        gap: 1em;
+        margin-top: 2em;
+    }
+    .editProfileForm{
+        display: grid;
+        place-items: center;
+        gap: 2em;
+    }
+    .input{
+        width: 280px;
+        height: 45px;
+        border-radius: 5px;
+        border: none;
+        text-align: center;
+    }
+
+    .submit{
+        background-color: #ff682c;
+        padding: 1em;
+        width: 150px;
+        border: none;
+        border-radius:  5px;
+    }
+
+</style>
